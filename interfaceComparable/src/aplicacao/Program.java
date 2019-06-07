0af537011f4c20053345c8aa8373e622edb3ae72
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-06-07 16:16:35 -0300

    Criacao do programa principal com lista
    Ainda não usada as interfaces comparable.

---
+++ interfaceComparable/src/aplicacao/Program.java
@@ -0,0 +1,32 @@
+package aplicacao;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<>();
+		String url = "C:\\Users\\Família Lustosa\\Documents\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(url))) {
+			String name = br.readLine();
+			while (name != null) {
+				list.add(name);
+				name = br.readLine();
+			}
+			Collections.sort(list);
+			for (String s : list) {
+				System.out.println(s);
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+
+}
