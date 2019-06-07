cf7d6d0d1f2c10e317110340d9ed71cf5d81fcb8
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-06-07 16:38:30 -0300

    Usando interface Comparable
    método sort para ordenar os funcionarios pelo nome

--- interfaceComparable/src/aplicacao/Program.java
+++ interfaceComparable/src/aplicacao/Program.java
@@ -7,22 +7,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import entidades.funcionarios;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<String> list = new ArrayList<>();
-		String url = "C:\\Users\\Família Lustosa\\Documents\\in.txt";
+		List<funcionarios> list = new ArrayList<>();
+		String url = "C:\\Users\\Família Lustosa\\Documents\\fun.txt";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(url))) {
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String funcionarioCSV = br.readLine();
+			
+			while (funcionarioCSV != null) {
+				String campos[] = funcionarioCSV.split(",");	//Cria vetor de duas posições usando a virgula do csv para separar as linhas em duas posições
+				list.add (new funcionarios(campos[0], Double.parseDouble(campos[1])));
+				funcionarioCSV = br.readLine();
 			}
 			Collections.sort(list);
-			for (String s : list) {
-				System.out.println(s);
+			for (funcionarios func : list) {
+				System.out.println(func.getNome() + ", " + func.getSalario());
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
---
+++ interfaceComparable/src/entidades/funcionarios.java
@@ -0,0 +1,37 @@
+package entidades;
+
+public class funcionarios implements Comparable<funcionarios> {
+	private String nome;
+	private Double Salario;
+	
+	public funcionarios(String nome, Double salario) {
+		this.nome = nome;
+		Salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return Salario;
+	}
+
+	public void setSalario(Double salario) {
+		Salario = salario;
+	}
+
+	@Override
+	public int compareTo(funcionarios outro) {	//Usando o metodo compareTo da interface comparable 
+		return nome.compareTo(outro.getNome());	//para comparar os funcionarios pelos seus nomes
+	}
+	
+	
+	
+	
+
+}
